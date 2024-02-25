d27ae056a666ff915397cf2eede67454b7ed6219
Sayaka Mitsutomo <126127285+aritan2525@users.noreply.github.com>
2024-02-25 09:57:15 +0900

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,22 @@
+export const middleware = (req: NextRequest) => {
+  const authorizationHeader = req.headers.get('authorization')
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(' ')[1]
+    const [user, password] = Buffer.from(basicAuth, 'base64').toString().split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Basic Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
